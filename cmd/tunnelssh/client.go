d7e9dab199ac0f09fc0772caffc304f9ea038e30
Force.Charlie <charlieio@outlook.com>
2020-05-20 23:02:24 +0800

    SSH Todo

--- cmd/tunnelssh/client.go
+++ cmd/tunnelssh/client.go
@@ -1,15 +1,110 @@
 package main
 
 import (
+	"bytes"
+	"log"
 	"net"
 	"os"
 	"strconv"
+	"strings"
 	"time"
+	"unicode/utf8"
 
 	"golang.org/x/crypto/ssh"
 )
 
-//
+const (
+	specialChars      = "\\'\"`${[|&;<>()*?!"
+	extraSpecialChars = " \t\n"
+	prefixChars       = "~"
+)
+
+func quote(word string, buf *bytes.Buffer) {
+	// We want to try to produce a "nice" output. As such, we will
+	// backslash-escape most characters, but if we encounter a space, or if we
+	// encounter an extra-special char (which doesn't work with
+	// backslash-escaping) we switch over to quoting the whole word. We do this
+	// with a space because it's typically easier for people to read multi-word
+	// arguments when quoted with a space rather than with ugly backslashes
+	// everywhere.
+	origLen := buf.Len()
+
+	if len(word) == 0 {
+		// oops, no content
+		buf.WriteString("''")
+		return
+	}
+
+	cur, prev := word, word
+	atStart := true
+	for len(cur) > 0 {
+		c, l := utf8.DecodeRuneInString(cur)
+		cur = cur[l:]
+		if strings.ContainsRune(specialChars, c) || (atStart && strings.ContainsRune(prefixChars, c)) {
+			// copy the non-special chars up to this point
+			if len(cur) < len(prev) {
+				buf.WriteString(prev[0 : len(prev)-len(cur)-l])
+			}
+			buf.WriteByte('\\')
+			buf.WriteRune(c)
+			prev = cur
+		} else if strings.ContainsRune(extraSpecialChars, c) {
+			// start over in quote mode
+			buf.Truncate(origLen)
+			goto quote
+		}
+		atStart = false
+	}
+	if len(prev) > 0 {
+		buf.WriteString(prev)
+	}
+	return
+
+quote:
+	// quote mode
+	// Use single-quotes, but if we find a single-quote in the word, we need
+	// to terminate the string, emit an escaped quote, and start the string up
+	// again
+	inQuote := false
+	for len(word) > 0 {
+		i := strings.IndexRune(word, '\'')
+		if i == -1 {
+			break
+		}
+		if i > 0 {
+			if !inQuote {
+				buf.WriteByte('\'')
+				inQuote = true
+			}
+			buf.WriteString(word[0:i])
+		}
+		word = word[i+1:]
+		if inQuote {
+			buf.WriteByte('\'')
+			inQuote = false
+		}
+		buf.WriteString("\\'")
+	}
+	if len(word) > 0 {
+		if !inQuote {
+			buf.WriteByte('\'')
+		}
+		buf.WriteString(word)
+		buf.WriteByte('\'')
+	}
+}
+
+// QuoteArgs todo
+func QuoteArgs(args []string) string {
+	var buf bytes.Buffer
+	for i := 0; i < len(args); i++ {
+		if i != 0 {
+			_ = buf.WriteByte(' ')
+		}
+		quote(args[i], &buf)
+	}
+	return buf.String()
+}
 
 // DialTunnel todo
 func DialTunnel(p, network, addr string, config *ssh.ClientConfig) (*ssh.Client, error) {
@@ -82,10 +177,24 @@ func (c *client) Shell() error {
 
 // Loop todo
 func (c *client) Loop() error {
+
+	c.sess.Stdout = os.Stdout
+	c.sess.Stderr = os.Stderr
 	if len(c.argv) == 0 {
+		stdin, err := c.sess.StdinPipe()
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer stdin.Close()
+		DebugPrint("ssh mode %s", c.host)
 		return c.Shell()
 	}
-	return nil
+	arg := QuoteArgs(c.argv)
+	//os.Stdin = c.sess.Stdin
+	if err := c.sess.Run(arg); err != nil {
+		return err
+	}
+	return c.sess.Wait()
 }
 
 // Dial todo
@@ -106,9 +215,6 @@ func (c *client) Dial() error {
 		return err
 	}
 	c.sess = sess
-	c.sess.Stdin = os.Stdin
-	c.sess.Stderr = os.Stderr
-	c.sess.Stdout = os.Stdout
 	return nil
 }
 
